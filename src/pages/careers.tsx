efc2a2ef0586846c981deab1d58b8d254834bd23
jonny-wg2 <jonny@wgtwo.com>
2022-01-29 20:11:59 +0100

    add careers from personio

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -10,6 +10,35 @@ const careers = [
 ];
 
 function Careers() {
+  let positionsList = []
+  fetch("https://wgtwo-jobs.personio.de/xml")
+  .then(response => response.text())
+  .then(str => new DOMParser().parseFromString(str, "text/xml"))
+  .then(xml => {
+      let positions = Array.from(xml.querySelectorAll("position")).map(p => ({
+          name: p.querySelector("name").innerHTML,
+          schedule: p.querySelector("schedule").innerHTML,
+          location: p.querySelector("office").innerHTML,
+          id: p.querySelector("id").innerHTML,
+      }));
+      console.log(positions)
+      positionsList = positions
+      window['_positions']= positions
+
+      positions.forEach(position => {
+        document.querySelector("#personio-ads").insertAdjacentHTML("beforeend", `
+            <a class=${styles.job} href="https://wgtwo-jobs.personio.de/job/${position.id}" target="_blank">
+                <div class="position-text">
+                    <div class=${styles.jobName}>${position.name}</div>
+                    <div class=${styles.jobLocation}>Permanent employee, ${position.schedule} - ${position.location}</div>
+                </div>
+                <ArrowRight />
+            </a>
+            
+        `)
+    })
+  });
+  console.dir(positionsList)
   return (
     <Layout title="Careers">
       <div className={common.page}>
@@ -17,6 +46,8 @@ function Careers() {
           <div className={common.container}>
             <div className={styles.jobs}>
               <div className={common.title}>Careers</div>
+              <div id="personio-ads">
+              </div>
               {careers.map((career) => (
                 <a
                   href={`/jobs/${career.name.toLowerCase().replaceAll(" ", "-")}`}
