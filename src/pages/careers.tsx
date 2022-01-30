051e3ab319f1cdec22826873b3ccda7fc8200fab
jonny-wg2 <jonny@wgtwo.com>
2022-01-30 17:44:38 +0100

    working without fetch for careers

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -10,35 +10,35 @@ const careers = [
 ];
 
 function Careers() {
-  let positionsList = []
-  fetch("https://wgtwo-jobs.personio.de/xml")
-  .then(response => response.text())
-  .then(str => new DOMParser().parseFromString(str, "text/xml"))
-  .then(xml => {
-      let positions = Array.from(xml.querySelectorAll("position")).map(p => ({
-          name: p.querySelector("name").innerHTML,
-          schedule: p.querySelector("schedule").innerHTML,
-          location: p.querySelector("office").innerHTML,
-          id: p.querySelector("id").innerHTML,
-      }));
-      console.log(positions)
-      positionsList = positions
-      window['_positions']= positions
+  // let positionsList = []
+  // fetch("https://wgtwo-jobs.personio.de/xml")
+  // .then(response => response.text())
+  // .then(str => new DOMParser().parseFromString(str, "text/xml"))
+  // .then(xml => {
+  //     let positions = Array.from(xml.querySelectorAll("position")).map(p => ({
+  //         name: p.querySelector("name").innerHTML,
+  //         schedule: p.querySelector("schedule").innerHTML,
+  //         location: p.querySelector("office").innerHTML,
+  //         id: p.querySelector("id").innerHTML,
+  //     }));
+  //     console.log(positions)
+  //     positionsList = positions
+  //     window['_positions']= positions
 
-      positions.forEach(position => {
-        document.querySelector("#personio-ads").insertAdjacentHTML("beforeend", `
-            <a class=${styles.job} href="https://wgtwo-jobs.personio.de/job/${position.id}" target="_blank">
-                <div class="position-text">
-                    <div class=${styles.jobName}>${position.name}</div>
-                    <div class=${styles.jobLocation}>Permanent employee, ${position.schedule} - ${position.location}</div>
-                </div>
-                <ArrowRight />
-            </a>
+  //     positions.forEach(position => {
+  //       document.querySelector("#personio-ads").insertAdjacentHTML("beforeend", `
+  //           <a class=${styles.job} href="https://wgtwo-jobs.personio.de/job/${position.id}" target="_blank">
+  //               <div class="position-text">
+  //                   <div class=${styles.jobName}>${position.name}</div>
+  //                   <div class=${styles.jobLocation}>Permanent employee, ${position.schedule} - ${position.location}</div>
+  //               </div>
+  //               <ArrowRight />
+  //           </a>
             
-        `)
-    })
-  });
-  console.dir(positionsList)
+  //       `)
+  //   })
+  // });
+  // console.dir(positionsList)
   return (
     <Layout title="Careers">
       <div className={common.page}>
