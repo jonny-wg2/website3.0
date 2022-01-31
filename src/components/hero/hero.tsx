ed4112dcac7f566cb0fef93d0dcde1cac03deb90
jonny-wg2 <jonny@wgtwo.com>
2022-01-31 15:06:31 +0100

    update hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -9,7 +9,7 @@ function Component() {
       <div className={`${common.container} ${common.splitContainer}`}>
         <div className={common.text}>
           <h1 className={styles.titleHeader}>
-            Programmable. <br /> Scalable. <br /> Flexible. <br /> Mobile core network.
+            Programmable. <br /> Scalable. <br /> Innovative. <br /> Mobile core network.
           </h1>
           <div className={common.subtitle}>
             No more boxes, no more end of life, no large upfront investments. With the <b>wgtwo</b> cloud native mobile core we offer continuous innovation, continuous development, scalability and flexibility. And to make it really simple for operators – delivered as a managed SaaS telecommunication network.
