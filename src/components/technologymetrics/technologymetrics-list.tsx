5497f88c4fe35c231f33d02b562fc8443492315c
jonny-wg2 <jonny@wgtwo.com>
2022-02-01 20:35:49 +0100

    update technology metrics

--- src/components/technologymetrics/technologymetrics-list.tsx
+++ src/components/technologymetrics/technologymetrics-list.tsx
@@ -5,7 +5,7 @@ import styles from "./technologymetrics.module.css";
 const technologyMetrics = [
   { name: "156.1 million", description: "SMSs",},
   { name: "321.6 million", description: "voice calls",},
-  { name: "8.3 million", description: "MMS",},
+  { name: "8.3 million", description: "MMSs",},
   { name: "1.8 petabyte", description: "data upload",},
   { name: "18.9 petabyte", description: "data download",},
   { name: "6.3 billion", description: "connectivity events",},
