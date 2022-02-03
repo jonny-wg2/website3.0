d8dee31c2bba808b1a1bc6ef810da29bea39a5e5
jonny-wg2 <jonny@wgtwo.com>
2022-02-03 08:01:42 +0100

    update config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,11 +2,12 @@ module.exports = {
   title: "Working Group Two",
   tagline:
     "No more boxes, no more end of life, no large upfront investments. With the wgtwo cloud native mobile core we offer continuous innovation, continuous development, scalability and flexibility. And to make it really simple for operators – delivered as a managed SaaS telecommunication network.",
-  url: "https://www.wgtwo.com",
+  url: "https://jonny-wg2.github.io/wgtwo",
   baseUrl: "/",
   favicon: "img/favicons/favicon.ico",
-  organizationName: "working-group-two",
-  projectName: "wgtwo.com",
+  organizationName: "jonny-wg2",
+  projectName: "website3.0",
+  deploymentBranch: "main",
   trailingSlash: "true",
   themeConfig: {
     metadata: [
