f2a71e399ebecee7a7cc2327aa9baccec00a84f9
jonny-wg2 <jonny@wgtwo.com>
2022-02-03 11:10:03 +0100

    add webhook

--- website/src/util/message.ts
+++ website/src/util/message.ts
@@ -1,6 +1,6 @@
 export default function sendMessage(message: string) {
   return fetch(
-    atob("aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDdCOTlMQkRNL0IwMzEwRjYxVUNDLzNsU0xVQVJmWk52YWlubXpKT0VUUEVGdAo="),
+    atob("$SLACK_WEBHOOK"),
     {
       method: "POST",
       body: `{"text":"${message.replace(/"/g, '\\"')}"}`,
