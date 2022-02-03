42cec087769c78da7491ec4d220a2f7409656fa4
jonny-wg2 <jonny@wgtwo.com>
2022-02-03 11:49:23 +0100

    remove image on footer

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Working Group Two",
   tagline:
     "No more boxes, no more end of life, no large upfront investments. With the wgtwo cloud native mobile core we offer continuous innovation, continuous development, scalability and flexibility. And to make it really simple for operators – delivered as a managed SaaS telecommunication network.",
-  url: "https://jonny-wg2.github.io/website3.0",
+  url: "https://jonny-wg2.github.io/",
   baseUrl: "/website3.0/",
   favicon: "img/favicons/favicon.ico",
   organizationName: "jonny-wg2",
@@ -81,11 +81,7 @@ module.exports = {
       style: "dark",
       links: [
         {
-          items: [
-            {
-              html: `<a href="/"><img alt="wgtwo Logo" src="/website3.0/img/logo_white.svg" width="281px" height="32px" class="footer-logo" /></a>`,
-            },
-          ],
+          // space for alignment to the right
         },
         {
           title: "Product",
